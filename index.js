7c046cc54d8ccc681b50e7be5ef8b04d791f5bc9
enrichosamb <187468259+enrichosamb@users.noreply.github.com>
2025-04-14 12:12:36 +0700

    feat: init files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+const port = 5050
+
+app.listen(port, () => console.log(`Server is running in port ${port}`))
+
+app.get("/test", (req, res, next) => {
+    try {
+        res.status(200).json({
+            message: "successfull to get api"
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: "error!"
+        })        
+    }
+})
