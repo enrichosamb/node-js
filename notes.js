923e5dbb88f283b4a4fc0dea93f0e8855d33d3aa
enrichosamb <Enricho@samb.co.id>
2025-09-22 14:22:36 +0700

    Update notes.js

--- notes.js
+++ notes.js
@@ -2,5 +2,6 @@
 
 docker buildx build --platform linux/amd64,linux/arm64 -t enrichosamb/node-js:latest --push .
 docker manifest inspect enrichosamb/node-js:latest
+test workflow ubuntu docker
 
 */
